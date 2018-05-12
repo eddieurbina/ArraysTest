85e7df4e0cf5cb4086e4755405524f5a77d07e6f
Alumno <Alumno@maq3>
2018-05-12 10:20:35 -0500

    Ciclos For

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,38 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int[][] arregloEntero = new int[10][10]; 
+        int n = 1; 
+                
+        for (int i = 0; i < 10; i++) { 
+            for (int j = 0; j < 10; j++) {
+                 arregloEntero[i][j] = n;
+                 n++;
+            }
+        }
+        
+        for (int i = 0; i < 10; i++) { 
+            for (int j = 0; j < 10; j++) { 
+                System.out.print("\t" + arregloEntero[i][j]);
+            }
+            System.out.println("");
+        }
+       /*
+        for (int i = 0; i<20; i++) { //Rellenar Variable 
+            arregloEntero[i] = i * 3;
+            if (arregloEntero[i]%2 != 0 ) 
+                System.out.println(arregloEntero[i]);
+        }
+        
+         for (int i = 0; i<20; i++) { 
+           // System.out.println(arregloEntero[i]);
+        }*/
+        
+        
+        
+        
+        
     }
     
 }
